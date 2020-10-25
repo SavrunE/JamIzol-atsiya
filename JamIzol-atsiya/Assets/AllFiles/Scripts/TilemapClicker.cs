c06f7e986bb3d839f0859ecd26cae63d2c391a38
SavrunE <igor1996igor@inbox.ru>
2020-10-25 13:28:10 +0300

    Flag to tile

---
+++ JamIzol-atsiya/Assets/AllFiles/Scripts/TilemapClicker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapClicker : MonoBehaviour
+{
+    public Transform PointerFlag;
+
+    public TileBase TileToSet;
+    private Tilemap map;
+    private Camera mainCamera;
+    void Start()
+    {
+        map = GetComponent<Tilemap>();
+        mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3Int clickCellPosition = map.WorldToCell(clickWorldPosition);
+
+
+            PointerFlag.position = map.CellToWorld(clickCellPosition);
+        }
+    }
+}
