740834433450106a1ad94a37a28a85d73904812e
SavrunE <igor1996igor@inbox.ru>
2020-10-26 13:45:47 +0300

    базовое меню, пока не работает

---
+++ JamIzol-atsiya/Assets/AllFiles/Scripts/SettingController/SettingController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingController : MonoBehaviour
+{
+    public event EventHandler OnStopTime;
+    private void Update()
+    {
+        
+    }
+}
