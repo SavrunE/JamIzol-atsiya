7149878c98244ca985c3fdacfbddd3b8d226f0fe
SavrunE <igor1996igor@inbox.ru>
2020-10-29 14:37:23 +0300

    флекс камеры готов

---
+++ JamIzol-atsiya/Assets/AllFiles/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent, RequireComponent(typeof(Camera))]
+public sealed class CameraControl : MonoBehaviour
+{
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            camera.transform.position -= new Vector3(Input.GetAxis("MouseX"), 0f, Input.GetAxis("MouseZ"));
+        }
+    }
+}
---
+++ JamIzol-atsiya/Assets/AllFiles/Scripts/Camera/MouseScrollWheel.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseScrollWheel : MonoBehaviour
+{
+    private Transform target;
+
+    [SerializeField]
+    private float scrollSpeed = 10.0f;
+    [SerializeField]
+    private float zoomMin = 1.0f;
+    [SerializeField]
+    private float zoomMax = 20.0f;
+    [SerializeField]
+    [Range(0.1f,1f)]
+    private float response = 0.5f;
+    
+    private float zoomEndPositionY;
+    private float point;
+    private void Start()
+    {
+        target = GetComponent<Transform>();
+    }
+    void LateUpdate()
+    {
+        if (target)
+        {
+            float moveScale = Input.GetAxis("Mouse ScrollWheel") * (-1);
+            float sqrMove = moveScale * moveScale;
+
+            if (moveScale != 0 && sqrMove < 1)
+            {
+                zoomEndPositionY = target.position.y + moveScale * scrollSpeed;
+                Debug.Log(zoomEndPositionY);
+                point = CheckZoom(zoomEndPositionY, zoomMin, zoomMax);
+                Vector3 endPosition = new Vector3(target.position.x, point, target.position.z);
+
+                target.position = Vector3.Lerp(transform.position, endPosition, response);
+            }
+        }
+    }
+    private static float CheckZoom(float zoomEndPosition, float min, float max)
+    {
+        if (zoomEndPosition < min)
+            zoomEndPosition = min;
+        if (zoomEndPosition > max)
+            zoomEndPosition = max;
+        return zoomEndPosition;
+    }
+}
